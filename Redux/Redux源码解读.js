efe582ee1f88b26841827b58095d817aa5ac34b6
xusanduo08 <2279190887@qq.com>
2018-06-10 21:54:06 +0800

    Redux源码解读

--- Redux/Redux源码解读.js
+++ Redux/Redux源码解读.js
@@ -1,7 +1,215 @@
 /*
 Redux:  createStore：创建并返回一个store
-        Provider：创建容器，该容器在上下文context中加入了store变量，使得内部的子组件都能通过上下文获取store
+        combineReducers: reducer合成。当发起一个Action时，该函数会根据相应的key值，将state数据传入到对应的子reducer中
         applymiddleware：返回一个函数，该函数先执行每个中间件到第一个return返回（即返回以next为入参的那一层），
                         然后将返回的函数一层层嵌套执行并返回最终结果，获得带有中间件功能的dispatch
 
 */
+
+/*
+    一个Redux应用只有一个单一的store
+    store通过Provider组件放在上下文中，子组件通过上下文获取store，这个子组件都是通过connect包装后生成的。
+*/
+import { storeShape, subscriptionShape } from "../utils/PropTypes";
+
+class Provider extends Component {
+    getChildContext() {
+        return {
+            store: this.store,
+            storeSubscription: null
+        }
+    }
+    constructor(props, context) {
+        super(props, context);
+        this.store = props.store;
+    }
+
+    render() {
+        return Children.only(this.props.children)
+    }
+}
+
+Provider.propTypes = {
+    store: storeShape.isRequired,
+    children: PropTypes.element.isRequired
+}
+
+Provider.childContextTypes = {
+    store: storeShape.isRequired,
+    storeSubscription: subscriptionShape
+}
+
+/*
+    connect()用来把组件包装成一个新的、连接到store上的组件
+*/
+
+connect: return createConnect()
+createConnect: return connect: return ConnectHOC(/*...args*/)
+ConnectHOC = connectAdvanced
+connectAdvanced: return wrapWithConnect(WrappedComponent)
+
+function connectAdvanced(
+    selectorFactory,
+    {
+        getDisplayName = name => `ConnectAdvanced(${name})`,
+        methodName = 'connectAdvanced',
+        renderCountProp = undefined,
+        shouldHandleStateChanges = true,
+        storeKey = 'store',
+        withRef = false,
+        ...connectOptions
+    } = {}
+) {
+    return function wrapWithConnect(WrappedComponent) {
+        class Connect extends Component {
+      //......  
+      return hoistStatics(Connect, WrappedComponent)
+    }
+}
+
+
+class Connect extends Component {
+    constructor(props, context) {
+        super(props, context)
+
+        this.version = version
+        this.state = {}
+        this.renderCount = 0
+        this.store = props[storeKey] || context[storeKey]
+        this.propsMode = Boolean(props[storeKey])
+        this.setWrappedInstance = this.setWrappedInstance.bind(this)
+
+        invariant(this.store,
+            `Could not find "${storeKey}" in either the context or props of ` +
+            `"${displayName}". Either wrap the root component in a <Provider>, ` +
+            `or explicitly pass "${storeKey}" as a prop to "${displayName}".`
+        )
+
+        this.initSelector()
+        this.initSubscription()
+    }
+
+    getChildContext() {
+        // If this component received store from props, its subscription should be transparent
+        // to any descendants receiving store+subscription from context; it passes along
+        // subscription passed to it. Otherwise, it shadows the parent subscription, which allows
+        // Connect to control ordering of notifications to flow top-down.
+        const subscription = this.propsMode ? null : this.subscription
+        return { [subscriptionKey]: subscription || this.context[subscriptionKey] }
+    }
+
+    componentDidMount() {
+        if (!shouldHandleStateChanges) return
+
+        // componentWillMount fires during server side rendering, but componentDidMount and
+        // componentWillUnmount do not. Because of this, trySubscribe happens during ...didMount.
+        // Otherwise, unsubscription would never take place during SSR, causing a memory leak.
+        // To handle the case where a child component may have triggered a state change by
+        // dispatching an action in its componentWillMount, we have to re-run the select and maybe
+        // re-render.
+        this.subscription.trySubscribe()
+        this.selector.run(this.props)
+        if (this.selector.shouldComponentUpdate) this.forceUpdate()
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.selector.run(nextProps)
+    }
+
+    shouldComponentUpdate() {
+        return this.selector.shouldComponentUpdate
+    }
+
+    componentWillUnmount() {
+        if (this.subscription) this.subscription.tryUnsubscribe()
+        this.subscription = null
+        this.notifyNestedSubs = noop
+        this.store = null
+        this.selector.run = noop
+        this.selector.shouldComponentUpdate = false
+    }
+
+    getWrappedInstance() {
+        invariant(withRef,
+            `To access the wrapped instance, you need to specify ` +
+            `{ withRef: true } in the options argument of the ${methodName}() call.`
+        )
+        return this.wrappedInstance
+    }
+
+    setWrappedInstance(ref) {
+        this.wrappedInstance = ref
+    }
+
+    initSelector() {
+        const sourceSelector = selectorFactory(this.store.dispatch, selectorFactoryOptions)
+        this.selector = makeSelectorStateful(sourceSelector, this.store)
+        this.selector.run(this.props)
+    }
+
+    initSubscription() {
+        if (!shouldHandleStateChanges) return
+
+        // parentSub's source should match where store came from: props vs. context. A component
+        // connected to the store via props shouldn't use subscription from context, or vice versa.
+        const parentSub = (this.propsMode ? this.props : this.context)[subscriptionKey]
+        this.subscription = new Subscription(this.store, parentSub, this.onStateChange.bind(this))
+
+        // `notifyNestedSubs` is duplicated to handle the case where the component is  unmounted in
+        // the middle of the notification loop, where `this.subscription` will then be null. An
+        // extra null check every change can be avoided by copying the method onto `this` and then
+        // replacing it with a no-op on unmount. This can probably be avoided if Subscription's
+        // listeners logic is changed to not call listeners that have been unsubscribed in the
+        // middle of the notification loop.
+        this.notifyNestedSubs = this.subscription.notifyNestedSubs.bind(this.subscription)
+    }
+
+    onStateChange() {
+        this.selector.run(this.props)
+
+        if (!this.selector.shouldComponentUpdate) {
+            this.notifyNestedSubs()
+        } else {
+            this.componentDidUpdate = this.notifyNestedSubsOnComponentDidUpdate
+            this.setState(dummyState)
+        }
+    }
+
+    notifyNestedSubsOnComponentDidUpdate() {
+        // `componentDidUpdate` is conditionally implemented when `onStateChange` determines it
+        // needs to notify nested subs. Once called, it unimplements itself until further state
+        // changes occur. Doing it this way vs having a permanent `componentDidUpdate` that does
+        // a boolean check every time avoids an extra method call most of the time, resulting
+        // in some perf boost.
+        this.componentDidUpdate = undefined
+        this.notifyNestedSubs()
+    }
+
+    isSubscribed() {
+        return Boolean(this.subscription) && this.subscription.isSubscribed()
+    }
+
+    addExtraProps(props) {
+        if (!withRef && !renderCountProp && !(this.propsMode && this.subscription)) return props
+        // make a shallow copy so that fields added don't leak to the original selector.
+        // this is especially important for 'ref' since that's a reference back to the component
+        // instance. a singleton memoized selector would then be holding a reference to the
+        // instance, preventing the instance from being garbage collected, and that would be bad
+        const withExtras = { ...props }
+        if (withRef) withExtras.ref = this.setWrappedInstance
+        if (renderCountProp) withExtras[renderCountProp] = this.renderCount++
+        if (this.propsMode && this.subscription) withExtras[subscriptionKey] = this.subscription
+        return withExtras
+    }
+
+    render() {
+        const selector = this.selector
+        selector.shouldComponentUpdate = false
+
+        if (selector.error) {
+            throw selector.error
+        } else {
+            return createElement(WrappedComponent, this.addExtraProps(selector.props))
+        }
+    }
+}
