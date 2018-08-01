858e6e27fbbe569471b19cd4645595d63209ac22
xusanduo08 <2279190887@qq.com>
2018-08-01 20:29:03 +0800

    增加测试代码

--- React-redux/Neo/src/reducer.js
+++ React-redux/Neo/src/reducer.js
@@ -1,7 +1,54 @@
+import {
+    ADD_TODO,
+    TOGGLE_TODO,
+    SET_VISIBILITY_FILTER,
+    VisibilityFilters
+  } from './actions.js'
 import {combineReducers} from "redux";
-
-function input(state, action){
-    return "aaa"
+const initialState = {
+    VisibilityFilter: VisibilityFilters.SHOW_ALL,
+    todos: [123,234]
 }
 
-export default combineReducers({input});
+const {SHOW_ALL} = VisibilityFilters;
+
+function VisibilityFilter(state= SHOW_ALL, action){
+    switch(action.type){
+        case SET_VISIBILITY_FILTER:
+            return action.filter
+        default:
+            return state
+    }
+}
+
+function todos(state=[], action){
+    switch(action.type){
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false
+                }
+            ]
+        case TOGGLE_TODO:
+            return state.map((todo, index) => {
+                if(index === action.index){
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    })
+                }
+                return todo;
+            })
+        default:
+            return state
+    }
+}
+
+const  todoApp = combineReducers({
+    VisibilityFilter,
+    todos
+})
+
+export default todoApp;
+
