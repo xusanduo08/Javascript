cf9502b829817899925f3a4c60833617e4a1da1e
xusanduo08 <2279190887@qq.com>
2018-08-01 23:00:32 +0800

    处理mapStateToProps和mapDispatchToProps为空的情况

---
+++ React-redux/Neo/src/dealMapDispatchToProps.js
@@ -0,0 +1,21 @@
+
+//处理mapDispatchToProps
+function dealMapDispatchToProps(mapDispatchToProps, dispatch){
+
+    //如果没有传入mapDispatchToProps参数，那么要确保dispatch方法作为props传入到组件中
+    if(!mapDispatchToProps){
+        return function constantSelector(dispatch, options){
+            return {dispatch}    
+        }
+    }
+
+    //如果传入function
+    if(typeof mapDispatchToProps === 'function'){
+        return function(dispatch){
+            return mapDispatchToProps(dispatch);
+        }
+    }
+}
+
+export default dealMapDispatchToProps;
+
---
+++ React-redux/Neo/src/dealMapStateToProps.js
@@ -0,0 +1,15 @@
+
+function dealMapStateToProps(mapStateToProps){
+    if(!mapStateToProps){
+        return function(){
+            return {};
+        }
+    }
+    if(typeof mapStateToProps === 'function'){
+        return function(state){
+            return mapStateToProps(state);
+        }
+    }
+}
+
+export default dealMapStateToProps;
