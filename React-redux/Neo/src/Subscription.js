678a6b808a205c803c2d505188ed55fd7164dc23
xusanduo08 <2279190887@qq.com>
2018-08-03 22:13:57 +0800

    抽取订阅动作到单独的类中

---
+++ React-redux/Neo/src/Subscription.js
@@ -0,0 +1,25 @@
+
+//订阅实例，用来订阅store，并管理子组件的定于
+
+export default class Subscription {
+  constructor(store, onStateChange) {
+    this.unsubscribe = null; // null代表没有订阅
+    this.store = store;
+    this.onStateChange = onStateChange;
+  }
+
+  //订阅
+  trySubscribe() {
+    if (!this.unsubscribe) {
+      this.unsubscribe = this.store.subscribe(this.onStateChange); //订阅后获获得一个取消订阅的函数
+    }
+  }
+
+  //取消订阅
+  tryUnsubscribe() {
+    if (this.unsubscribe) {
+      this.unsubscribe(); // 取消订阅
+    }
+  }
+
+}
--- React-redux/Neo/src/connect.js
+++ React-redux/Neo/src/connect.js
@@ -2,6 +2,7 @@ import { Component, createElement } from 'react';
 import { PropTypes } from 'prop-types';
 import dealMapDispatchToProps from './dealMapDispatchToProps.js'
 import dealMapStateToProps from './dealMapStateToProps.js';
+import Subscription from './Subscription';
 /*
   react-redux的主要功能就是让react的组件与redux的store关联起来，也就是订阅到store上。
   这份工作靠的主要就是connect方法。
@@ -15,48 +16,50 @@ import dealMapStateToProps from './dealMapStateToProps.js';
 
 function connect(mapStateToProps, mapDispatchToProps) {
 
-    const initMapDispatchToProps = dealMapDispatchToProps(mapDispatchToProps);
-    const initMapStateToProps = dealMapStateToProps(mapStateToProps);
-    return function (component) {
+  const initMapDispatchToProps = dealMapDispatchToProps(mapDispatchToProps);
+  const initMapStateToProps = dealMapStateToProps(mapStateToProps);
+  return function (component) {
 
-        class Connect extends Component {
-            constructor(props, context) {
-                super(props, context);
-                this.store = this.context.store; //从上下文中获取store，由Provider将store放到上下文中
-                if(Boolean(mapStateToProps)){
-                    this.init();  // 如果mapStateToProps没有传递的话，则connect组件不会去订阅store的变化。
-                }
-                
-            }
-
-            componentDidMount(){
-                this.store.subscribe(this.onStateChange.bind(this));
-            }
-
-            init() {
-                //从store中拿到state，并用mapStateToProsp、mapDispatchToProps计算出要传入component的props
-                this.stateProps = initMapStateToProps(this.store.getState());
-                this.dispatchProps = initMapDispatchToProps(this.store.dispatch);
-            }
-
-            onStateChange() {
-                //state如果发生变化，就要重新计算props值，并让组件re-render。
-                this.stateProps = initMapStateToProps(this.store.getState());
-                this.dispatchProps = initMapDispatchToProps(this.store.dispatch);
-                this.setState({});  // 仅为触发组件re-render过程。
-            }
-
-            render() {
-                return createElement(component, { ...this.stateProps, ...this.dispatchProps });
-            }
-
+    class Connect extends Component {
+      constructor(props, context) {
+        super(props, context);
+        this.store = this.context.store; //从上下文中获取store，由Provider将store放到上下文中
+        if (Boolean(mapStateToProps)) {
+          this.init();  // 如果mapStateToProps没有传递的话，则connect组件不会去订阅store的变化。
         }
+        this.subscription = new Subscription(this.store, this.onStateChange.bind(this)); // 抽取订阅动作到订阅实例中，便于管理
+      }
 
-        Connect.contextTypes = {
-            store: PropTypes.object
+      componentDidMount() {
+        if (Boolean(mapStateToProps)) {
+          this.subscription.trySubscribe()
         }
-        return Connect;
+      }
+
+      init() {
+        //从store中拿到state，并用mapStateToProsp、mapDispatchToProps计算出要传入component的props
+        this.stateProps = initMapStateToProps(this.store.getState());
+        this.dispatchProps = initMapDispatchToProps(this.store.dispatch);
+      }
+
+      onStateChange() {
+        //state如果发生变化，就要重新计算props值，并让组件re-render。
+        this.stateProps = initMapStateToProps(this.store.getState());
+        this.dispatchProps = initMapDispatchToProps(this.store.dispatch);
+        this.setState({});  // 仅为触发组件re-render过程。
+      }
+
+      render() {
+        return createElement(component, { ...this.stateProps, ...this.dispatchProps });
+      }
+
     }
+
+    Connect.contextTypes = {
+      store: PropTypes.object
+    }
+    return Connect;
+  }
 }
 
 
