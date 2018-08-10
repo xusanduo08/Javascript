7b17e48f02d4e867406a1472644e7be9b10e7e37
xusanduo08 <2279190887@qq.com>
2018-08-10 23:20:53 +0800

    加入单元测试

---
+++ React-redux/Neo/connect.test.js
@@ -0,0 +1,91 @@
+import React, { Children, Component } from 'react'
+import createClass from 'create-react-class'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import TestRenderer from 'react-test-renderer'
+import { createStore } from 'redux'
+import connect from './src/connect'
+
+describe('connect', () => {
+  class Passthrough extends Component {
+    render() {
+      return <div />
+    }
+  }
+
+  class ProviderMock extends Component {
+    getChildContext() {
+      return { store: this.props.store }
+    }
+
+    render() {
+      return Children.only(this.props.children)
+    }
+  }
+  ProviderMock.childContextTypes = {
+    store: PropTypes.object.isRequired
+  }
+
+  class ContextBoundStore {
+    constructor(reducer) {
+      this.reducer = reducer
+      this.listeners = []
+      this.state = undefined
+      this.dispatch({})
+    }
+
+    getState() {
+      return this.state
+    }
+
+    subscribe(listener) {
+      this.listeners.push(listener)
+      return (() => this.listeners.filter(l => l !== listener))
+    }
+
+    dispatch(action) {
+      this.state = this.reducer(this.getState(), action)
+      this.listeners.forEach(l => l())
+      return action
+    }
+  }
+
+  function stringBuilder(prev = '', action) {
+    return action.type === 'APPEND'
+      ? prev + action.body
+      : prev
+  }
+
+  function imitateHotReloading(TargetClass, SourceClass, container) {
+    // Crude imitation of hot reloading that does the job
+    Object.getOwnPropertyNames(SourceClass.prototype).filter(key =>
+      typeof SourceClass.prototype[key] === 'function'
+    ).forEach(key => {
+      if (key !== 'render' && key !== 'constructor') {
+        TargetClass.prototype[key] = SourceClass.prototype[key]
+      }
+    })
+
+    container.forceUpdate()
+  }
+
+  it('should receive the store in the context', () => {
+    const store = createStore(() => ({}))
+
+    @connect()
+    class Container extends Component {
+      render() {
+        return <Passthrough {...this.props} />
+      }
+    }
+
+    const testRenderer = TestRenderer.create(
+      <ProviderMock store={store}>
+        <Container pass="through" />
+      </ProviderMock>
+    )
+
+    const container = testRenderer.root.findByType(Container)
+    expect(container.instance.context.store).toBe(store)
+  })
+})
