f8ab17aa2cbc82fe49b1fc2cb2b11a0ce0ecc96f
xusanduo08 <2279190887@qq.com>
2019-10-27 21:54:54 +0800

    增加count属性，记录子孙元素的数量

--- VirtualDOM/createElement.js
+++ VirtualDOM/createElement.js
@@ -1,11 +1,18 @@
 
 export default function createElement(type, config, ...args){
-  const props = Object.assign({}, config);
+  const props = Object.assign({count: 0}, config);
   const hasChildren = args.length > 0;
   const rawChildren = hasChildren ? [].concat(...args) : [];
-
+  
   props.children = rawChildren.filter(c => c != null && c != false)
-    .map(c => c instanceof Object ? c: createTextElement(c));
+    .map(c => {
+      if(c instanceof Object){
+        props.count += c.props.count; // count属性记录了当前节点下所有子节点的数量，包括孙子节点、曾孙子节点等等哦~
+      } else {
+        props.count ++;
+      }
+      return c instanceof Object ? c: createTextElement(c);
+    });
   return {type, props};
 }
 
