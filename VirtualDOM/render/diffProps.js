7c3816fec3edc7ec30bf59fc1e96de86a58a720a
xusanduo08 <2279190887@qq.com>
2019-11-05 19:29:32 +0800

    diffProps

---
+++ VirtualDOM/render/diffProps.js
@@ -0,0 +1,25 @@
+export default function diffProps(oldNode, newNode){
+  // 比较属性的变更
+  // 已经不存在的属性，要删除
+  // 属性值已经改变的，要更新
+  // 新增的属性，要添加
+  // 直接返回一个对象，这个对象上的所有属性都要添加到oldNode上
+  let oldProps = oldNode.props;
+  let newProps = newNode.props;
+  let flag = false;
+
+  let propsPatches = {};
+  for(let key in newProps){
+    let value = newProps[key];
+
+    if(oldProps.hasOwnProperty(key) && oldProps[key] !== value && key !== 'children'){ // oldNode上也存在这个属性，但属性值已经变更 -> 更新属性值
+      flag = true;
+      propsPatches[key] = value;
+    } else if(!oldProps.hasOwnProperty(key) && key !== 'children'){ // 新增的属性
+      flag = true;
+      propsPatches[key] = value;
+    }
+  }
+
+  return  flag ? propsPatches : null; // 没有属性变更的话返回null
+}
