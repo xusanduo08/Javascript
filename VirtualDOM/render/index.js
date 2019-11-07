1c10ac6828cadd4c9a476a4886cfddc57cadaa2f
xusanduo08 <2279190887@qq.com>
2019-11-07 22:00:37 +0800

    测试

--- VirtualDOM/render/index.js
+++ VirtualDOM/render/index.js
@@ -1,15 +1,20 @@
 import createElement from './createElement';
 import render from './render';
 import diff from './diff';
+import applyPatches from './applyPatches';
 
 /**@jsx createElement */
 
-let dom = <div id='id'>1234</div>
-let newDOM = <div id='id'><span></span></div>
-console.log(diff(dom, newDOM));
+let dom = <div id='id'><span>span</span><div>div</div></div>
+let newDOM = <div id='id'><span>123</span></div>
+
+let patches = diff(dom, newDOM);
+console.log(patches);
 let root = document.createElement('div');
 root.setAttribute('id', 'root');
 document.body.appendChild(root);
-render(dom, root);
+dom = render(dom, root);
+
+applyPatches(dom, patches);
 
 
